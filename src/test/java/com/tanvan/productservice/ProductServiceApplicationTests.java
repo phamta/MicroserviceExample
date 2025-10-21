1726c8c00e1413cd8d1fac4fceb55de2ecc322df
Tanvan <tanvan.13022014@gmail.com>
2025-10-21 21:50:47 +0700

    Write Integration Test

--- src/test/java/com/tanvan/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/tanvan/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,57 @@
 package com.tanvan.productservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
 class ProductServiceApplicationTests {
 
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.4");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void testCreateProduct() throws Exception {
+        String productJson = """
+            {
+                "name": "Iphone 15",
+                "description": "New Apple phone",
+                "price": 1200
+            }
+        """;
+
+        mockMvc.perform(post("/api/product/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productJson))
+                .andExpect(status().isCreated()); // Kiểm tra trả về 201
+    }
+
+    @Test
+    void testGetAllProducts() throws Exception {
+        mockMvc.perform(get("/api/product"))
+                .andExpect(status().isOk());
     }
 
 }
