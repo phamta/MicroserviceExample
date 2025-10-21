ba18ddc6f5eec8ae290858e38cb0744b85bf859a
Tanvan <tanvan.13022014@gmail.com>
2025-10-21 19:49:02 +0700

    Create Product Service

---
+++ src/main/java/com/tanvan/productservice/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.tanvan.productservice.controller;
+
+import com.tanvan.productservice.dto.ProductRequest;
+import com.tanvan.productservice.dto.ProductResponse;
+import com.tanvan.productservice.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+
+}
---
+++ src/test/java/com/tanvan/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanvan.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
